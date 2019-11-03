06aaa866941f7826e4ff4f5ccfee0ab884a291c9
Glitch (hello-webpage) <none>
2019-11-03 23:15:23 +0000

    🎹🛬 Checkpoint ./script.js:110795/1397 ./index.html:110795/43

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 import * as owp from "https://cdn.jsdelivr.net/npm/owp@0.1.2/dist/index.js";
 
-owp.fromEvent(document.querySelector("#aperture input"), "input")
-  .pipeThrough(owp.map(ev => ev.target.value)
-  .pipeThrough(owp.map(v =>  Math.sqrt(2 ** (v/3))).toFixed(1))
+function myRound(v) {
+  if(v%1 >= .6) {
+    return Math.ceil(v);
+  }
+  return Math.floor(v);
+}
+
+function textContentSink(el) {
+  return new WritableStream({
+    write(v) {
+      el.textContent = v;
+    }
+  })
+}
+
+export function fromInput(el) {
+  const { next, observable } = owp.external();
+  el.addEventListener("input", () => next(el.value));
+  next(el.value);
+  return observable;
+}
+
+fromInput(document.querySelector("#aperture input"))
+  .pipeThrough(owp.map(v => (myRound(Math.sqrt(2 ** (v/3)) * 10)/10).toFixed(1)))
+  .pipeTo(textContentSink(document.querySelector("#aperture .output")))
+
+fromInput(document.querySelector("#focal input"))
+  .pipeTo(textContentSink(document.querySelector("#focal .output")))
