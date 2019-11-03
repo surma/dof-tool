8ec6b387c6b00ac9873ac0ee4b1cdb46906e61ba
Glitch (hello-webpage) <none>
2019-11-03 23:50:23 +0000

    🌋👨‍👨‍👦 Checkpoint ./script.js:110795/790

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function myRound(v) {
   return Math.floor(v);
 }
 
-function setTextContent(el) {
+function setTextContent(el, f) {
   return v => {
-    el.textContent = v;
+    el.textContent = f(v);
   };
 }
 
@@ -20,6 +20,13 @@ export function fromInput(el) {
   return observable;
 }
 
+function fork(v, f) {
+  const [s1, s2] = v.tee();
+  f(s1);
+  return s2;
+}
+
+fork(
 owp.combineLatest(
   fromInput(document.querySelector("#aperture input"))
     .pipeThrough(owp.map(v => myRound(Math.sqrt(2 ** (v/3)) * 10)/10))
@@ -29,15 +36,16 @@ owp.combineLatest(
   fromInput(document.querySelector("#focal input"))
     .pipeThrough(owp.forEach(setTextContent(document.querySelector("#focal .output"))))
 )
-  .pipeThrough(owp.map(([aperture, focal]) => (focal * focal / (aperture * 0.03) + focal)/1000))
-  .pipeThrough(owp.map(v => v.toFixed(1)))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#hyperfocal .output"))))
+  .pipeThrough(owp.map(([aperture, focal]) => ({aperture, focal})))
+  .pipeThrough(owp.map(({aperture, focal}) => ({aperture, focal, hyperfocal: ((focal * focal / (aperture * 0.03) + focal)/1000).toFixed(1)})))
+  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#hyperfocal .output"), ({hyperfocal}) => hyperfocal)))
   .pipeThrough(owp.combineLatestWith(
     fromInput(document.querySelector("#distance input"))
       .pipeThrough(owp.forEach(setTextContent(document.querySelector("#distance .output"))))
   ))
-  .pipeThrough(owp.map(([[a, b], c]) => [a, b, c]))
-  .pipeThrough(owp.map(([aperture, focal, distance]) => 2 * distance * distance * aperture * 0.03 / (focal * focal)))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#dof .output"))))
+  // .pipeThrough(owp.forEach(v => console.log(v)))
+  // .pipeThrough(owp.map(([[a, b], c]) => [a, b, c]))
+  // .pipeThrough(owp.map(([aperture, focal, distance]) => 2 * distance * distance * aperture * 0.03 / (focal * focal) / 1000))
+  // .pipeThrough(owp.forEach(setTextContent(document.querySelector("#dof .output"))))
   .pipeTo(owp.discard());
 
