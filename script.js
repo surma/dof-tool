638699ecf7e02194f4970e7ff734c0caa0ef533f
Glitch (hello-webpage) <none>
2019-11-03 23:03:24 +0000

    🕌🗽 Checkpoint ./script.js:110795/214

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as owp from "https://cdn.jsdelivr.net/npm/owp@0.1.2/dist/index.js";
 
 owp.fromEvent(document.querySelector("#aperture input"), "input")
-  .pipeThrough(owp.map()
+  .pipeThrough(owp.map(ev => ev.target.value)
+  .pipeThrough(owp.map(v =>  Math.sqrt(2 ** (v/3))).toFixed(1))
