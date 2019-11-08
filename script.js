49f39ee1655970e12d0de7e85895078df38118a3
Surma <surma@surma.link>
2019-11-08 16:19:25 +0000

    Some work on the flight

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-import "https://unpkg.com/web-streams-polyfill@2.0.5/dist/polyfill.es2018.mjs";
-import * as owp from "https://cdn.jsdelivr.net/npm/owp@0.1.2/dist/index.js";
+import "./node_modules/web-streams-polyfill/dist/polyfill.es2018.mjs";
+import * as owp from "./node_modules/owp/dist/index.js";
 import ScrollSlider from "./scroll-slider.js";
 
-customElements.define('scroll-slider', ScrollSlider);
+customElements.define("scroll-slider", ScrollSlider);
 
 function myRound(v) {
-  if(v%1 >= .6) {
+  if (v % 1 >= 0.6) {
     return Math.ceil(v);
   }
   return Math.floor(v);
@@ -38,66 +38,152 @@ export function fromChange(el) {
 }
 
 function formatDistance(v) {
-  if(v < 0) {
+  if (v < 0) {
     return "∞";
   }
   return `${(v / 1000).toFixed(2)}m`;
 }
 
-owp.combineLatest(
-  owp.just(0.03), // CoC
-  owp.merge(
-    fromInput(document.querySelector("#aperture input.slider"))
-      .pipeThrough(owp.map(v => myRound(Math.sqrt(2 ** (v/3)) * 10)/10)),
-    fromChange(document.querySelector("#aperture input.field"))
-      .pipeThrough(owp.map(v => myRound(Math.sqrt(2**(Math.round(Math.log2(v**2)*3)/3))*10)/10))
+owp
+  .combineLatest(
+    owp.just(0.03), // CoC
+    owp
+      .merge(
+        fromInput(document.querySelector("#aperture input.slider")).pipeThrough(
+          owp.map(v => myRound(Math.sqrt(2 ** (v / 3)) * 10) / 10)
+        ),
+        fromChange(document.querySelector("#aperture input.field")).pipeThrough(
+          owp.map(
+            v =>
+              myRound(
+                Math.sqrt(2 ** (Math.round(Math.log2(v ** 2) * 3) / 3)) * 10
+              ) / 10
+          )
+        )
+      )
+      .pipeThrough(owp.distinct())
+      .pipeThrough(
+        owp.forEach(setValue(document.querySelector("#aperture input.slider")))
+      )
+      .pipeThrough(
+        owp.forEach(setValue(document.querySelector("#aperture input.field")))
+      ),
+    owp
+      .merge(
+        fromInput(document.querySelector("#focal input.slider")),
+        fromChange(document.querySelector("#focal input.field"))
+      )
+      .pipeThrough(owp.map(v => Number(v)))
+      .pipeThrough(owp.distinct())
+      .pipeThrough(
+        owp.forEach(setValue(document.querySelector("#focal input.slider")))
+      )
+      .pipeThrough(
+        owp.forEach(setValue(document.querySelector("#focal input.field")))
+      ),
+    owp
+      .merge(
+        fromInput(document.querySelector("#distance input.slider")),
+        fromChange(document.querySelector("#distance input.field"))
+      )
+      .pipeThrough(owp.distinct())
+      .pipeThrough(
+        owp.forEach(setValue(document.querySelector("#distance input.slider")))
+      )
+      .pipeThrough(
+        owp.forEach(setValue(document.querySelector("#distance input.field")))
+      )
+      .pipeThrough(owp.map(v => v * 1000))
   )
-    .pipeThrough(owp.distinct())
-    .pipeThrough(owp.forEach(setValue(document.querySelector("#aperture input.slider"))))
-    .pipeThrough(owp.forEach(setValue(document.querySelector("#aperture input.field")))),
-  owp.merge(
-    fromInput(document.querySelector("#focal input.slider")),
-    fromChange(document.querySelector("#focal input.field"))
+  .pipeThrough(
+    owp.map(([coc, aperture, focal, distance]) => ({
+      coc,
+      aperture,
+      focal,
+      distance
+    }))
   )
-    .pipeThrough(owp.map(v => Number(v)))
-    .pipeThrough(owp.distinct())
-    .pipeThrough(owp.forEach(setValue(document.querySelector("#focal input.slider"))))
-    .pipeThrough(owp.forEach(setValue(document.querySelector("#focal input.field")))),
-  owp.merge(
-    fromInput(document.querySelector("#distance input.slider")),
-    fromChange(document.querySelector("#distance input.field"))
+  .pipeThrough(
+    owp.map(data => {
+      const { aperture, focal, coc } = data;
+      const hyperfocal = focal ** 2 / (aperture * coc) + focal;
+      return { ...data, hyperfocal };
+    })
   )
-    .pipeThrough(owp.distinct())
-    .pipeThrough(owp.forEach(setValue(document.querySelector("#distance input.slider"))))
-    .pipeThrough(owp.forEach(setValue(document.querySelector("#distance input.field"))))
-    .pipeThrough(owp.map(v => v * 1000))
-)
-  .pipeThrough(owp.map(([coc, aperture, focal, distance]) => ({coc, aperture, focal, distance})))
-  .pipeThrough(owp.map(data => {
-    const {aperture, focal, coc} = data;
-    const hyperfocal = (focal**2 / (aperture * coc) + focal);
-    return {...data, hyperfocal};
-  }))
-  .pipeThrough(owp.map(data => {
-    const {distance, focal, hyperfocal} = data;
-    const nearFocusPlane = distance * (hyperfocal - focal) / (hyperfocal + distance - 2*focal);
-    const farFocusPlane = distance * (hyperfocal - focal) / (hyperfocal - distance);
-    return {...data, farFocusPlane, nearFocusPlane};
-  }))
-  .pipeThrough(owp.map(data => {
-    const {distance, nearFocusPlane, farFocusPlane } = data;
-    const dofNear = distance - nearFocusPlane;
-    const dofFar = farFocusPlane - distance;
-    const totalDof = dofNear + dofFar;
-    return {...data, dofNear, dofFar, totalDof};
-  }))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#hyperfocal .output"), ({hyperfocal}) => formatDistance(hyperfocal))))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#output #totalDof tspan"), ({totalDof}) => formatDistance(totalDof))))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#output #nearFocusPlane tspan"), ({nearFocusPlane}) => formatDistance(nearFocusPlane))))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#output #farFocusPlane tspan"), ({farFocusPlane}) => formatDistance(farFocusPlane))))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#output #dofNear tspan"), ({dofNear}) => formatDistance(dofNear))))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#output #dofFar tspan"), ({dofFar}) => formatDistance(dofFar))))
-  .pipeThrough(owp.forEach(setTextContent(document.querySelector("#output #distance tspan"), ({distance}) => formatDistance(distance))))
+  .pipeThrough(
+    owp.map(data => {
+      const { distance, focal, hyperfocal } = data;
+      const nearFocusPlane =
+        (distance * (hyperfocal - focal)) / (hyperfocal + distance - 2 * focal);
+      const farFocusPlane =
+        (distance * (hyperfocal - focal)) / (hyperfocal - distance);
+      return { ...data, farFocusPlane, nearFocusPlane };
+    })
+  )
+  .pipeThrough(
+    owp.map(data => {
+      const { distance, nearFocusPlane, farFocusPlane } = data;
+      const dofNear = distance - nearFocusPlane;
+      const dofFar = farFocusPlane - distance;
+      const totalDof = dofNear + dofFar;
+      return { ...data, dofNear, dofFar, totalDof };
+    })
+  )
+  .pipeThrough(
+    owp.forEach(
+      setTextContent(
+        document.querySelector("#hyperfocal .output"),
+        ({ hyperfocal }) => formatDistance(hyperfocal)
+      )
+    )
+  )
+  .pipeThrough(
+    owp.forEach(
+      setTextContent(
+        document.querySelector("#output #totalDof tspan"),
+        ({ totalDof }) => formatDistance(totalDof)
+      )
+    )
+  )
+  .pipeThrough(
+    owp.forEach(
+      setTextContent(
+        document.querySelector("#output #nearFocusPlane tspan"),
+        ({ nearFocusPlane }) => formatDistance(nearFocusPlane)
+      )
+    )
+  )
+  .pipeThrough(
+    owp.forEach(
+      setTextContent(
+        document.querySelector("#output #farFocusPlane tspan"),
+        ({ farFocusPlane }) => formatDistance(farFocusPlane)
+      )
+    )
+  )
+  .pipeThrough(
+    owp.forEach(
+      setTextContent(
+        document.querySelector("#output #dofNear tspan"),
+        ({ dofNear }) => formatDistance(dofNear)
+      )
+    )
+  )
+  .pipeThrough(
+    owp.forEach(
+      setTextContent(
+        document.querySelector("#output #dofFar tspan"),
+        ({ dofFar }) => formatDistance(dofFar)
+      )
+    )
+  )
+  .pipeThrough(
+    owp.forEach(
+      setTextContent(
+        document.querySelector("#output #distance tspan"),
+        ({ distance }) => formatDistance(distance)
+      )
+    )
+  )
   // .pipeThrough(owp.forEach(v => console.log(new Date().getTime(), v)))
   .pipeTo(owp.discard());
-
--- scroll-slider.js
+++ scroll-slider.js
@@ -1,7 +1,11 @@
 export default class ScrollSlider extends HTMLElement {
+  static get observedAttributes() {
+    return ["snap", "num-items"];
+  }
+
   constructor() {
     super();
-    this.attachShadow({mode: 'open'});
+    this.attachShadow({ mode: "open" });
     this.shadowRoot.innerHTML = `
       <style>
         :host {
@@ -12,18 +16,20 @@ export default class ScrollSlider extends HTMLElement {
           width: 100%;
           height: 100%;
           overflow: auto;
-          scroll-snap-type: x mandatory;
           display: flex;
-          padding-bottom: var(--scrollbar-margin, 20px);
-          margin-bottom: calc(-1 * var(--scrollbar-margin, 20px));
+          /* padding-bottom: var(--scrollbar-margin, 20px); */
+          /* margin-bottom: calc(-1 * var(--scrollbar-margin, 20px));*/
+        }
+        .snap {
+          scroll-snap-type: x mandatory;
         }
         #crosshair {
           position: absolute;
           left: 50%;
           height: calc(100% - 2px);
-          transform: translateX(-50%);
-          width: calc(2 * var(--spacing, 3em));
-          border: 1px solid red;
+          //transform: translateX(-50%);
+          //width: var(--spacing, 3em);
+          border-left: 1px solid red;
           pointer-events: none;
         }
         .padding {
@@ -33,8 +39,19 @@ export default class ScrollSlider extends HTMLElement {
         }
         .label {
           scroll-snap-align: center;
-          padding: 0 var(--spacing, 3em);
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          width: var(--spacing, 3em);
+          flex-shrink: 0;
+          //transform: translateX(-50%);
         } 
+        .label:first-of-type {
+          margin-left: calc(-.5 * var(--spacing, 3em));
+        }
+        .label:last-of-type {
+          margin-right: calc(-.5 * var(--spacing, 3em));
+        }
       </style>
       <div id="crosshair"></div>
       <div id="container">
@@ -42,23 +59,78 @@ export default class ScrollSlider extends HTMLElement {
         <div class="padding"></div>
       </div>
     `;
-    
-    this._labelFunction = x => x;
-    this._container = this.shadowRoot.querySelector('#container');
-    this._numItems = 10;
+
+    this._valueFunction = x => x;
+    this._container = this.shadowRoot.querySelector("#container");
+    this._regenerateLabels();
+    this._processNumItemsAttribute();
+
+    this._container.addEventListener("scroll", this._onScroll.bind(this));
+  }
+
+  attributeChangedCallback() {
+    this._processSnapAttribute();
+    this._processNumItemsAttribute();
+  }
+
+  get snap() {
+    return this.hasAttribute("snap");
+  }
+
+  set snap(value) {
+    if (Boolean(value)) {
+      this.setAttribute("snap", "");
+    } else {
+      this.removeAttribute("snap");
+    }
+  }
+
+  get numItems() {
+    return this._numItems;
+  }
+
+  set numItems(value) {
+    this._numItems = value;
+    this._regenerateLabels();
+    this._onScroll();
+  }
+
+  set valueFunction(f) {
+    this._valueFunction = f;
     this._regenerateLabels();
   }
-  
+
+  _processSnapAttribute() {
+    this._container.classList.toggle("snap", this.snap);
+  }
+
+  _processNumItemsAttribute() {
+    const attrValue = parseInt(this.getAttribute("num-items") || "10", 10);
+    if (attrValue !== this.numItems) {
+      this.numItems = attrValue;
+      this._regenerateLabels();
+    }
+  }
+
   _regenerateLabels() {
-    this._container.querySelectorAll('*:not(.padding)').forEach(el => el.remove());
-    const lastChild = this._container.querySelector('*:last-of-type');
-    for(let i = 0; i < 10; i++) {
-      const span = document.createElement("span")
-      span.classList.add('label');
-      span.textContent = `${i}`;
+    this._container
+      .querySelectorAll("*:not(.padding)")
+      .forEach(el => el.remove());
+    const lastChild = this._container.querySelector("*:last-of-type");
+    for (let i = 0; i < this.numItems; i++) {
+      const span = document.createElement("span");
+      span.classList.add("label");
+      span.textContent = `${this._valueFunction(i)}`;
       this._container.insertBefore(span, lastChild);
     }
-  }  
-  
-  
+  }
+
+  _onScroll() {
+    const offset =
+      (this._container.scrollLeft /
+        (this._container.scrollWidth - this._container.clientWidth)) *
+      (this._numItems - 1);
+    const value = this._valueFunction(offset);
+    console.log(value);
+  }
 }
